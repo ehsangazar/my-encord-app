4e714b93933e56151af3d8f7b63405fc3344978f
Ehsan Gazar <me@ehsangazar.com>
2024-03-12 06:15:42 +0100

    fixed outside click on modal

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Button from "../Button/Button";
 import FormGroup from "../FormGroup/FormGroup";
 
@@ -10,11 +11,39 @@ interface ModalProps {
 }
 
 const Modal = ({ title, children, open, onClose, description }: ModalProps) => {
+  const modalRef = useRef(null);
+
+  const clickOutsideModal = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      onClose();
+    }
+  };
+
+  const pressEscape = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", clickOutsideModal);
+    document.addEventListener("keydown", pressEscape);
+    return () => {
+      document.removeEventListener("mousedown", clickOutsideModal);
+      document.removeEventListener("keydown", pressEscape);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   if (!open) return null;
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 px-4">
-      <div className="bg-white p-4 rounded-lg min-w-full sm:min-w-96">
+      <div
+        ref={modalRef}
+        className="bg-white p-4 rounded-lg min-w-full sm:min-w-96"
+      >
         <h2 className="text-2xl font-bold mb-4">{title}</h2>
         {description && <p className="mb-4">{description}</p>}
         {children}
